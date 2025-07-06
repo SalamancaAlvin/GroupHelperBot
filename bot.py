225261b6da255330d9d49b7a6d605f9bf2b2457a
SalamancaAlvin <Salamancaalvin3@gmail.com>
2025-07-06 16:21:14 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,13 @@
+
 import logging
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import os
 
 logging.basicConfig(level=logging.INFO)
 
 TOKEN = '8079599334:AAE2-azAjR-_tSV9HirqMb2_tjhEpI9HSOU'
 URL = 'https://grouphelperbot.onrender.com'
+PORT = int(os.environ.get("PORT", 10000))
 
 def start(update, context):
     logging.info('Comando /start recibido')
@@ -22,14 +25,18 @@ def unknown(update, context):
 def main():
     logging.info('Iniciando el bot...')
     updater = Updater(TOKEN, use_context=True)
-    logging.info('Bot iniciado correctamente')
     dp = updater.dispatcher
     dp.add_handler(CommandHandler('start', start))
     dp.add_handler(MessageHandler(Filters.all, unknown))
-    logging.info('Handlers agregados correctamente')
-    updater.start_webhook(listen="0.0.0.0", port=443, url_path='webhook', webhook_url=URL + '/webhook')
+    updater.start_webhook(
+        listen="0.0.0.0",
+        port=PORT,url_path='webhook',
+        webhook_url=URL + '/webhook'
+    )
     logging.info('Webhook iniciado correctamente')
     updater.idle()
 
-if __name__ == '__main__':
+if _name_ == '_main_':
     main()
+
+
