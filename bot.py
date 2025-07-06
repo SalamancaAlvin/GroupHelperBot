1f969c6f0deb94dd22ffd048ffeff0d9d45cef16
SalamancaAlvin <Salamancaalvin3@gmail.com>
2025-07-06 16:35:33 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,42 +1,35 @@
-
 import logging
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-import os
+from flask import Flask, request
+from telegram import Bot, Update
+from telegram.ext import Dispatcher, CommandHandler, MessageHandler, Filters
+
+TOKEN = '8079599334:AAE2-azAjR-_tSV9HirqMb2_tjhEpI9HSOU'
+bot = Bot(token=TOKEN)
+
+app = Flask(_name_)
 
 logging.basicConfig(level=logging.INFO)
 
-TOKEN = '8079599334:AAE2-azAjR-_tSV9HirqMb2_tjhEpI9HSOU'
-URL = 'https://grouphelperbot.onrender.com'
-PORT = int(os.environ.get("PORT", 10000))
+dispatcher = Dispatcher(bot=bot, update_queue=None, use_context=True)
 
 def start(update, context):
-    logging.info('Comando /start recibido')
-    logging.info(f'Chat ID: {update.effective_chat.id}')
-    try:
-        context.bot.send_message(chat_id=update.effective_chat.id, text='¡Hola! Soy CrimsonMeguBot')
-        logging.info('Mensaje enviado correctamente')
-    except Exception as e:
-        logging.error(f'Error al enviar mensaje: {e}')
+    context.bot.send_message(chat_id=update.effective_chat.id, text='¡Hola! Soy CrimsonMeguBot')
 
 def unknown(update, context):
-    logging.info('Mensaje recibido')
-    logging.info(f'Texto del mensaje: {update.message.text}')
+    context.bot.send_message(chat_id=update.effective_chat.id, text='Comando no reconocido.')
 
-def main():
-    logging.info('Iniciando el bot...')
-    updater = Updater(TOKEN, use_context=True)
-    dp = updater.dispatcher
-    dp.add_handler(CommandHandler('start', start))
-    dp.add_handler(MessageHandler(Filters.all, unknown))
-    updater.start_webhook(
-        listen="0.0.0.0",
-        port=PORT,url_path='webhook',
-        webhook_url=URL + '/webhook'
-    )
-    logging.info('Webhook iniciado correctamente')
-    updater.idle()
+dispatcher.add_handler(CommandHandler('start', start))
+dispatcher.add_handler(MessageHandler(Filters.command, unknown))
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    update = Update.de_json(request.get_json(force=True), bot)
+    dispatcher.process_update(update)
+    return 'ok'
+
+@app.route('/')
+def home():
+    return 'Bot en funcionamiento'
 
 if _name_ == '_main_':
-    main()
-
-
+    app.run(port=10000)
